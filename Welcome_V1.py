d617bbcd44a118297c3c33c90cc3534c5856ba4c
MichaelWinder <winderm2@middleton.school.nz>
2024-05-08 09:41:39 +1200

    starting Welcome page

--- Welcome_V1.py
+++ Welcome_V1.py
@@ -1,1 +1,13 @@
-import tkinter
+from tkinter import *
+
+root = Tk()
+
+root.title("Maori Quiz!")
+
+root.geometry("+2500+100")
+root.minsize(800, 500)
+root.resizable(False, False)
+root.configure(bg='steel blue')
+Label(root, text="Welcome to the Maori Quiz", bg="steel blue",
+      fg="white", font=("Arial Black", 40, "bold")).grid(row=0, column=3)
+mainloop()
