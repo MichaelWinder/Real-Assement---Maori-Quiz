bac36975b3e53219e7cccf23aef56c6fb2915d4f
MichaelWinder <winderm2@middleton.school.nz>
2024-05-08 10:30:36 +1200

    update

--- Welcome_V1.py
+++ Welcome_V1.py
@@ -1,13 +1,47 @@
 from tkinter import *
 
-root = Tk()
 
-root.title("Maori Quiz!")
+def instructions(itype):
+    if itype == "welcome":
+        return "Click on one of the five buttons below.\nThe " \
+               "Multiple choice button gives 10 multiple choice questions.\n" \
+               "The True or False button gives 10 True or false questions.\n" \
+               "The Whole word button gives 10 questions that require a " \
+               "whole word answer.\nClick on Results to get your results.\n" \
+               "Click on Exit to end the program."
+    elif itype == "multi":
+        pass
+    elif itype == "trueorfalse":
+        pass
+    elif itype == "whole":
+        pass
 
-root.geometry("+2500+100")
-root.minsize(800, 500)
-root.resizable(False, False)
-root.configure(bg='steel blue')
-Label(root, text="Welcome to the Maori Quiz", bg="steel blue",
-      fg="white", font=("Arial Black", 40, "bold")).grid(row=0, column=3)
+
+welcome = Tk()
+
+welcome.title("Maori Quiz!")
+
+welcome.geometry("+2500+100")
+welcome.minsize(800, 500)
+welcome.resizable(False, False)
+welcome.configure(bg='steel blue')
+# Text Labels for Welcome Screen
+Label(welcome, text="Welcome to the Maori Quiz!", bg="chocolate2",
+      fg="white", font=("Arial Black", 40, "bold")).grid(row=0,
+                                                         columnspan=3)
+Label(welcome, text="Instructions", bg="steel blue", fg="white",
+      font=("Arial Black", 25, "bold")).grid(row=1, column=1, pady=10)
+Label(welcome, text=instructions("welcome"), bg="steel blue", fg="white",
+      font=("Arial Black", 10)).grid(row=2, column=1, pady=0)
+# Buttons for Welcome Screen
+multi = Button(welcome, text="Multiple Choice", command="multiple()")
+multi.grid(row=3, column=0)
+torf = Button(welcome, text="True or False", command="t_or_f()")
+torf.grid(row=3, column=1)
+whole = Button(welcome, text="Whole Word", command="whole_w()")
+whole.grid(row=3, column=2)
+results = Button(welcome, text="Results", command="results()")
+results.grid(row=4, column=1, padx=20)
+exit = Button(welcome, text="Exit", command="exit()")
+exit.grid(row=4, column=1, padx=10)
 mainloop()
